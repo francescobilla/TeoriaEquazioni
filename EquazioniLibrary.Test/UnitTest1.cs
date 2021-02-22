edaeea3cef24c2525b8514ff7926150e155df6a4
francescobilla <francesco.billa2004@gmail.com>
2021-02-22 09:17:12 +0100

    aggiunta secondo esercizio

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -7,8 +7,21 @@ namespace EquazioniLibrary.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsDeterminedTest()
         {
+            double a = 0;//5,0
+            bool risultato_aspettato = false;//true, false
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risultato_aspettato, risp); 
+        }
+        [TestMethod]
+        public void IsInconsistedTest()
+        {
+            double a = 10;//0,10
+            double b = 3;//0,3
+            bool risultato_aspettato = false;
+            bool risp = Equazioni.IsInconsisted(a,b);
+            Assert.AreEqual(risultato_aspettato, risp);
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -4,9 +4,21 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
-        public static bool IsDetermined(double a)// true equazione determinata
+        public static bool IsDetermined(double a)// true equazione determinata a>0
         {
+            bool determinata = false;     
+            if (a>0)
+            {
+                return determinata = true;
+            }
+            return determinata;
+        }
+        public static bool IsInconsisted(double a, double b)// true equazione impossibile a=0, b=0
+        {
+            if (a == 0 && b == 0)
+                return true;
 
+            return false;
         }
     }
 }
