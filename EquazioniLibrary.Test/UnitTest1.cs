e3208bcfbb101c97d54d4af3044ffca6be41d34b
francescobilla <francesco.billa2004@gmail.com>
2021-02-23 12:53:11 +0100

    aggiunta esercizi

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -23,5 +23,33 @@ namespace EquazioniLibrary.Test
             bool risp = Equazioni.IsInconsisted(a,b);
             Assert.AreEqual(risultato_aspettato, risp);
         }
+        [TestMethod]
+        public void IsDegree2Test()
+        {
+            double esponente = 3;//1, 3
+            bool risultato_aspettato = true;// false, true
+            bool risp = Equazioni.IsDegree2(esponente);
+            Assert.AreEqual(risultato_aspettato, risp);
+        }
+        [TestMethod]
+        public void DeltaTest()
+        {
+            double a = 2;//3,2 test nullo 2
+            double b = 4;//4,4 test nullo 4
+            double c = 2;//7,0 test nullo 2
+            double risultato_aspettato = 0;//-68,16 test nullo 0
+            double risp = Equazioni.Delta(a,b,c);
+            Assert.AreEqual(risultato_aspettato, risp);
+        }
+        [TestMethod]
+        public void Soluzioni()
+        {
+            double a = 2;//2,3,2
+            double b = 4;//4,4,4
+            double c = 0;//2,7,0
+            string risultato_aspettato = "la soluzione è: 0 e -2";//"la soluzione è: -1 e -1","impossibile","la soluzione è: 0 e -2"
+            string risp = Equazioni.Soluzioni(a, b, c);
+            Assert.AreEqual(risultato_aspettato, risp);
+        }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -20,5 +20,32 @@ namespace EquazioniLibrary
 
             return false;
         }
+        public static bool IsDegree2(double esponente)// false se l'equazione non è di secondo grado
+        {
+            if(esponente == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static double Delta(double a, double b, double c)
+        {
+            double delta = (b * b) + (-4 * a * c);
+            return delta;
+        }
+       public static string Soluzioni(double a, double b, double c)
+        {
+            double delta = Delta(a, b, c);
+            if (delta < 0)
+            {
+                string s = "impossibile";
+                return s;
+            }
+            double x1 = ((-1 * b) + Math.Sqrt(delta)) / (2 * a);
+            double x2 = ((-1 * b) - Math.Sqrt(delta)) / (2 * a);
+            string x1_x2= $"la soluzione è: {x1} e {x2}";
+            return x1_x2;
+        }
+
     }
 }
